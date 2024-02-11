998cd9b3ac3ed7fd22761243a789c163bf11fb02
viorel556 <viorel.harabaru@gmail.com>
2024-02-11 20:44:48 +0200

    Exploring Vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-
-
+  import MyHello from '@/components/MyHello.vue';
 </script>
 
 <template>
-
-
+  <h1>Viorel Harabaru</h1>
+  <MyHello/>
 </template>
 
 
-<style scoped></style>
+
+<style> </style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <!--  HERE IS THE LAYOUT ITSELF -->
+
+  <p class="text">HERE IS AN IMPORTED COMPONENT</p>
+  <button>Click Me Bitch!</button>
+
+
+</template>
+
+
+<style scoped>
+  body { background-color: red; }
+  .text { background-color: aquamarine}
+
+</style>
