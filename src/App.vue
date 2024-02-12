ce3cd1241f56074fb53aebe5c0b913c4eaa6995a
viorel556 <viorel.harabaru@gmail.com>
2024-02-12 16:46:08 +0200

    Passing props; Binding; Slots;

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-  <MyHello />
+  <!-- SENDING PROPS HERE: -->
+  <!-- ":" - indicates to Vue that this is a property of dinamically interepreted type -->
+  <MyHello title="Sneakers Puma" :price="1200" is-added/>
+  <MyHello title="Sneakers Nike" :price="1500"/>
+
+  <MyHello title="Sneakers Nike" :price="1500">
+    
+    <!-- BELLOW IS A SLOT CONTENT:  -->
+    <button> Additional Button </button>
+  </MyHello> 
+
 </template>
 
 
--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -1,20 +1,31 @@
 <script setup>
-import { ref } from 'vue';
-// import see from "../utils/myJsUtils"; 
 
-// COMPOSITION API: 
-let count = ref(0);
-
-function increment() {
-    count.value++;
-}
-
+defineProps({
+  // IN VUE, tipization is required for the received props;
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
 
 </script>
 
 <template>
-    <!--  HERE IS THE LAYOUT ITSELF -->
-
-    <h1>{{ count }}</h1>
-    <button @click="increment"> + </button>
+  <!--  HERE IS THE LAYOUT ITSELF -->
+  <div class="product">
+    <h2>Product: {{ title }}</h2>
+    <h3>Price: {{ price }} </h3>
+    <slot>
+      <!-- AN ADDITIONAL BLANK SPACE FOR A DIV -->
+      <p>THIS IS SOME DEFAULT CONTENT IN CASE NOTHING IS PROVIDED</p>
+    </slot>
+  </div>
 </template>
+
+
+<style>
+.product {
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
